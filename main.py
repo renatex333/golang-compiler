56b6e53fb9db3909665f32b879e7a0c00dc21a81
Renatex333 <renato9302@hotmail.com>
2023-08-21 17:59:26 -0300

    Quase que foi

--- main.py
+++ main.py
@@ -1,86 +1,82 @@
 import sys
-import operator
 
-valid_operations = {'+': operator.add, '-': operator.sub}
+class Token:
+    def __init__(self, type:str, value:int):
+        self.type = type
+        self.value = value
 
-def preprocess(arg):
-    string = arg
-    string = string.replace("\\n","")
-    string = string.replace("\n","")
-    return string
+class Tokenizer:
+    def __init__(self, source:str, position:int, next:Token):
+        self.source = source
+        self.position = position
+        self.next = next
 
-def lexical_analysis(s):
-    for i in range(len(s)):
-        if (s[i].isnumeric()) or (s[i] in valid_operations) or (s[i] == " "):
-            continue
+    def select_next(self):
+        token_type = None
+        token_value = None
+        if self.source[self.position].isnumeric():
+            token_type = "INTEGER"
+            number = str(self.source[self.position])
+            while self.source[self.position+1].isnumeric():
+                number += str(self.source[self.position+1])
+                self.position += 1
+            token_value = int(self.source[self.position])
+        elif self.source[self.position] == "+":
+            token_type = "PLUS"
+            token_value = self.source[self.position]
+            self.position += 1
+        elif self.source[self.position] == "-":
+            token_type = "MINUS"
+            token_value = self.source[self.position]
+            self.position += 1
+        elif self.source[self.position] == " ":
+            self.position += 1
+            self.select_next()
+        elif self.position == len(self.source):
+            token_type = "EOF"
+            token_value = ""
         else:
-            raise Exception(f"Invalid input: {repr(s[i])} in {repr(s)}")
+            raise Exception("Invalid input")
+        self.next = Token(token_type, token_value)
 
-def syntactic_analysis(s):
-    pass
+class Parser:
+    tokenizer = None
+    @staticmethod
+    def parse_expression():
+        result = None
+        if Parser.tokenizer.next.type == "INTEGER":
+            result = Parser.tokenizer.next.value
+            Parser.tokenizer.select_next()
+            while Parser.tokenizer.next.type == "PLUS" or Parser.tokenizer.next.type == "MINUS":
+                if Parser.tokenizer.next.type == "PLUS":
+                    Parser.tokenizer.select_next()
+                    if Parser.tokenizer.next.type == "INTEGER":
+                        result += Parser.tokenizer.next.value
+                    else:
+                        raise Exception("Invalid input")
+                elif Parser.tokenizer.next.type == "MINUS":
+                    Parser.tokenizer.select_next()
+                    if Parser.tokenizer.next.type == "INTEGER":
+                        result -= Parser.tokenizer.next.value
+                    else:
+                        raise Exception("Invalid input")
+                else:
+                    raise Exception("Invalid input")
+                Parser.tokenizer.select_next()
+            return result
+        else:
+            raise Exception("Invalid input")
 
-def semantic_analysis(s):
-    for i in range(len(s)):
-        # Checa se um operador está entre dois números
-        if s[i] in valid_operations:
-            if i == 0 or i == len(s)-1:
-                raise Exception(f"Invalid input: {repr(s[i])} in {repr(s)}")
-            
-            for j in range(i+1, len(s)):
-                if s[j] == " ":
-                    continue
-                if s[j].isnumeric():
-                    break
-                if s[j] in valid_operations:
-                    raise Exception(f"Invalid input: {repr(s[i])} in {repr(s)}")
-            else:
-                raise Exception(f"Invalid input: {repr(s[i])} in {repr(s)}")
-            
-            for j in range(i-1, -1, -1):
-                if s[j] == " ":
-                    continue
-                if s[j].isnumeric():
-                    break
-                if s[j] in valid_operations:
-                    raise Exception(f"Invalid input: {repr(s[i])} in {repr(s)}")
-            else:
-                raise Exception(f"Invalid input: {repr(s[i])} in {repr(s)}")
-            
-        # Checa se não há espaços entre os números
-        elif s[i] == " " and s[i-1].isnumeric():
-            for j in range(i+1, len(s)):
-                if s[j] == " ":
-                    continue
-                if s[j] in valid_operations:
-                    break
-                if s[j].isnumeric():
-                    raise Exception(f"Invalid input: {repr(s[i])} in {repr(s)}")
-
-def calculate(s):
-    operands = []
-    operators = []
-    number = ""
-    for i in range(len(s)):
-        if s[i].isnumeric():
-            number += s[i]
-        elif s[i] in valid_operations:
-            operands.append(int(number))
-            operators.append(s[i])
-            number = ""
-        if i == len(s)-1:
-            operands.append(int(number))
-    
-    result = operands[0]
-    for operand, operator in zip(operands[1:], operators):
-        result = valid_operations[operator](result, operand)
-    return result
+    @staticmethod
+    def run(code):
+        Parser.tokenizer = Tokenizer(code, 0, None)
+        result = Parser.parse_expression()
+        if Parser.tokenizer.next.type != "EOF":
+            raise Exception("Invalid input")
+        return result
 
 def main(argv):
-    string = preprocess(argv[1])
-    lexical_analysis(string)
-    syntactic_analysis(string)
-    semantic_analysis(string)
-    result = calculate(string)
+    result = Parser.run(argv[1])
     print(result)
 
 if __name__ == "__main__":

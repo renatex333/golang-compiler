6cb6406b760049d91bea6441c6f465921ffdd73a
renatex333 <renato9302@hotmail.com>
2023-09-04 17:25:42 -0300

    Adicionando parse_factor de forma a lidar com operadores unários e parênteses

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Tokenizer:
     def select_next(self):
         token_type = None
         token_value = None
-        if self.position == len(self.source) or self.source[self.position] == "#":
+        if self.position == len(self.source):
             token_type = "EOF"
             token_value = ""
             self.next = Token(token_type, token_value)
@@ -50,6 +50,16 @@ class Tokenizer:
         elif self.source[self.position] == " ":
             self.position += 1
             self.select_next()
+        elif self.source[self.position] == "(":
+            token_type = "LEFTPARENTESIS"
+            token_value = "("
+            self.position += 1
+            self.next = Token(token_type, token_value)
+        elif self.source[self.position] == ")":
+            token_type = "RIGHTPARENTESIS"
+            token_value = ")"
+            self.position += 1
+            self.next = Token(token_type, token_value)
         else:
             raise Exception(f"Invalid Token Error: Token {self.source[self.position]} is not a valid token.")
 
@@ -57,49 +67,68 @@ class Parser:
     tokenizer = None
 
     @staticmethod
-    def parse_term():
-        result = None
+    def run(code):
+        Parser.tokenizer = Tokenizer(code, 0, None)
         Parser.tokenizer.select_next()
-        if Parser.tokenizer.next.type == "INT":
-            result = Parser.tokenizer.next.value
-            Parser.tokenizer.select_next()
-            while (Parser.tokenizer.next.type == "MULT") or (Parser.tokenizer.next.type == "DIV"):
-                if Parser.tokenizer.next.type == "MULT":
-                    Parser.tokenizer.select_next()
-                    if Parser.tokenizer.next.type == "INT":
-                        result *= Parser.tokenizer.next.value
-                    else:
-                        raise Exception(f"Invalid Input Error: Expected an integer after operation. Got '{Parser.tokenizer.next.value}' instead.")
-                elif Parser.tokenizer.next.type == "DIV":
-                    Parser.tokenizer.select_next()
-                    if Parser.tokenizer.next.type == "INT":
-                        result //= Parser.tokenizer.next.value
-                    else:
-                        raise Exception(f"Invalid Input Error: Expected an integer after operation. Got '{Parser.tokenizer.next.value}' instead.")
-                Parser.tokenizer.select_next()
-            return result
-        else:
-            raise Exception(f"Invalid Input Error: Expected an integer at the beginning of a term. Got '{Parser.tokenizer.next.value}' instead.")
-        
+        result = Parser.parse_expression()
+        if Parser.tokenizer.next.type != "EOF":
+            raise Exception(f"Invalid Input Error: Last Token type is not EOF, is {Parser.tokenizer.next.type}.")
+        return result
+
     @staticmethod
     def parse_expression():
         result = None
         result = Parser.parse_term()
         while (Parser.tokenizer.next.type == "PLUS") or (Parser.tokenizer.next.type == "MINUS"):
             if Parser.tokenizer.next.type == "PLUS":
+                Parser.tokenizer.select_next()
                 result += Parser.parse_term()
             elif Parser.tokenizer.next.type == "MINUS":
+                Parser.tokenizer.select_next()
                 result -= Parser.parse_term()
         return result
 
     @staticmethod
-    def run(code):
-        Parser.tokenizer = Tokenizer(code, 0, None)
-        result = Parser.parse_expression()
-        if Parser.tokenizer.next.type != "EOF":
-            raise Exception(f"Invalid Input Error: Last Token type is not EOF, is {Parser.tokenizer.next.type}.")
+    def parse_term():
+        result = None
+        result = Parser.parse_factor()
+        while (Parser.tokenizer.next.type == "MULT") or (Parser.tokenizer.next.type == "DIV"):
+            if Parser.tokenizer.next.type == "MULT":
+                Parser.tokenizer.select_next()
+                if Parser.tokenizer.next.type == "INT":
+                    result *= Parser.tokenizer.next.value
+                else:
+                    raise Exception(f"Invalid Input Error: Expected an integer after operation. Got '{Parser.tokenizer.next.value}' instead.")
+            elif Parser.tokenizer.next.type == "DIV":
+                Parser.tokenizer.select_next()
+                if Parser.tokenizer.next.type == "INT":
+                    result //= Parser.tokenizer.next.value
+                else:
+                    raise Exception(f"Invalid Input Error: Expected an integer after operation. Got '{Parser.tokenizer.next.value}' instead.")
+            Parser.tokenizer.select_next()
         return result
 
+    @staticmethod
+    def parse_factor():
+        result = None
+        if Parser.tokenizer.next.type == "INT":
+            result = Parser.tokenizer.next.value
+            Parser.tokenizer.select_next()
+        elif Parser.tokenizer.next.type == "PLUS":
+            Parser.tokenizer.select_next()
+            result = Parser.parse_factor()
+        elif Parser.tokenizer.next.type == "MINUS":
+            Parser.tokenizer.select_next()
+            result = -Parser.parse_factor()
+        elif Parser.tokenizer.next.type == "LEFTPARENTESIS":
+            Parser.tokenizer.select_next()
+            result = Parser.parse_expression()
+            if Parser.tokenizer.next.type != "RIGHTPARENTESIS":
+                raise Exception(f"Invalid Input Error: Expected ')' after expression. Got '{Parser.tokenizer.next.value}' instead.")
+            Parser.tokenizer.select_next()
+        else:
+            raise Exception(f"Invalid Input Error: Expected an integer, '+', '-', or '('. Got '{Parser.tokenizer.next.value}' instead.")
+        return result
 def main(argv):
     result = Parser.run(argv[1])
     print(result)

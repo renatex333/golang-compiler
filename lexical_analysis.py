1b63d8683df8bfb40b1887d9d8d1bcf7b1ca516d
renatex333 <renato9302@hotmail.com>
2023-09-18 20:02:33 -0300

    Adicionando tratamento de novos Tokens

--- lexical_analysis.py
+++ lexical_analysis.py
@@ -26,6 +26,16 @@ class Tokenizer:
                 self.position += 1
             token_value = int(number)
             self.next = Token(token_type, token_value)
+        elif self.source[self.position].isalpha():
+            token_type = "IDENTIFIER"
+            token_value = str(self.source[self.position])
+            self.position += 1
+            while self.position < len(self.source) and (self.source[self.position].isalpha() or self.source[self.position].isnumeric() or self.source[self.position] == "_"):
+                token_value += str(self.source[self.position])
+                self.position += 1
+            if token_value == "Println":
+                token_type = "PRINT"
+            self.next = Token(token_type, token_value)
         elif self.source[self.position] == "+":
             token_type = "PLUS"
             token_value = "+"
@@ -46,9 +56,6 @@ class Tokenizer:
             token_value = "/"
             self.position += 1
             self.next = Token(token_type, token_value)
-        elif self.source[self.position] == " ":
-            self.position += 1
-            self.select_next()
         elif self.source[self.position] == "(":
             token_type = "LEFTPARENTESIS"
             token_value = "("
@@ -59,6 +66,19 @@ class Tokenizer:
             token_value = ")"
             self.position += 1
             self.next = Token(token_type, token_value)
+        elif self.source[self.position] == "\n":
+            token_type = "NEWLINE"
+            token_value = "\\n"
+            self.position += 1
+            self.next = Token(token_type, token_value)
+        elif self.source[self.position] == "=":
+            token_type = "ASSIGNMENT"
+            token_value = "="
+            self.position += 1
+            self.next = Token(token_type, token_value)
+        elif self.source[self.position] == " ":
+            self.position += 1
+            self.select_next()
         else:
             raise Exception(
                 f"Invalid Token Error: Token {self.source[self.position]} is not a valid token. Located at position {self.position}.")

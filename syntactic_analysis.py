ec3da7bc1e6075a80131a545eed86d9a5c15f964
renatex333 <renato9302@hotmail.com>
2023-09-18 20:03:11 -0300

    Adicionando novos tipos de nós e Tabela de Símbolos

--- syntactic_analysis.py
+++ syntactic_analysis.py
@@ -1,49 +1,91 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 
+class SymbolTable:
+    def __init__(self):
+        self.table = dict()
+    
+    def get(self, identifier: str):
+        try:
+            return self.table[identifier]
+        except KeyError:
+            raise Exception(f"Identifier Error: Identifier '{identifier}' not declared.")
+    
+    def set(self, identifier: str, value: int):
+        self.table[identifier] = value
+
 class Node(ABC):
     def __init__(self, value: str, children: list[Node]):
         self.value = value
         self.children = children
 
     @abstractmethod
-    def evaluate(self):
+    def evaluate(self, symbol_table: SymbolTable):
         pass
 
 class BinOp(Node):
     def __init__(self, value: str, children: list[Node]):
         super().__init__(value, children)
 
-    def evaluate(self):
+    def evaluate(self, symbol_table: SymbolTable):
         if self.value == "+":
-            return self.children[0].evaluate() + self.children[1].evaluate()
+            return self.children[0].evaluate(symbol_table) + self.children[1].evaluate(symbol_table)
         elif self.value == "-":
-            return self.children[0].evaluate() - self.children[1].evaluate()
+            return self.children[0].evaluate(symbol_table) - self.children[1].evaluate(symbol_table)
         elif self.value == "*":
-            return self.children[0].evaluate() * self.children[1].evaluate()
+            return self.children[0].evaluate(symbol_table) * self.children[1].evaluate(symbol_table)
         elif self.value == "/":
-            return self.children[0].evaluate() // self.children[1].evaluate()
+            return self.children[0].evaluate(symbol_table) // self.children[1].evaluate(symbol_table)
 
 class UnOp(Node):
     def __init__(self, value: str, children: list[Node]):
         super().__init__(value, children)
 
-    def evaluate(self):
+    def evaluate(self, symbol_table: SymbolTable):
         if self.value == "+":
-            return self.children[0].evaluate()
+            return self.children[0].evaluate(symbol_table)
         elif self.value == "-":
-            return -self.children[0].evaluate()
+            return -self.children[0].evaluate(symbol_table)
 
 class IntVal(Node):
     def __init__(self, value: str, children: list[Node]):
         super().__init__(value, children)
 
-    def evaluate(self):
+    def evaluate(self, symbol_table: SymbolTable):
         return int(self.value)
+    
+class Identifier(Node):
+    def __init__(self, value: str, children: list[Node]):
+        super().__init__(value, children)
+
+    def evaluate(self, symbol_table: SymbolTable):
+        return symbol_table.get(self.value)
+    
+class Assignment(Node):
+    def __init__(self, value: str, children: list[Node]):
+        super().__init__(value, children)
+
+    def evaluate(self, symbol_table: SymbolTable):
+        symbol_table.set(self.children[0].value, self.children[1].evaluate(symbol_table))
+
+class Print(Node):
+    def __init__(self, value: str, children: list[Node]):
+        super().__init__(value, children)
+
+    def evaluate(self, symbol_table: SymbolTable):
+        print(self.children[0].evaluate(symbol_table))
+
+class Block(Node):
+    def __init__(self, value: str, children: list[Node]):
+        super().__init__(value, children)
+
+    def evaluate(self, symbol_table: SymbolTable):
+        for child in self.children:
+            child.evaluate(symbol_table)
 
 class NoOp(Node):
     def __init__(self, value: str, children: list[Node]):
         super().__init__(value, children)
 
-    def evaluate(self):
+    def evaluate(self, symbol_table: SymbolTable):
         pass

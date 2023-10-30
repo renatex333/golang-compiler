39323021947972be49ee3e1556c0bc2e46331cb2
renatex333 <renato9302@hotmail.com>
2023-10-30 17:42:10 -0300

    Criando CodeGenerator e implementando criação de .asm com sucesso

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import sys
 
 from compiler.src.parser.Parser import Parser
 from compiler.src.semantic_analysis.SymbolTable import SymbolTable
+from compiler.src.codegen.CodeGenerator import CodeGen
 
 def main(argv):
     root = Parser.run(argv[1])
     symbol_table = SymbolTable()
-    root.evaluate(symbol_table)
+    filename = "program.asm"
+    if len(argv) > 2:
+        filename = argv[2]
+    code_gen = CodeGen(filename)
+    code_gen.start()
+    # root.evaluate(symbol_table, code_gen)
+    code_gen.finish()
 
 if __name__ == "__main__":
     main(sys.argv)
